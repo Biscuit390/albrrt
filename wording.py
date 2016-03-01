2a50876911599c2457afad5715e53abd7d26b82b
Biscuit390 <biscuit390@gmail.com>
2016-03-01 22:14:15 +0000

    Completed working on reading words, starting work on trim function

--- wording.py
+++ wording.py
@@ -1,5 +1,6 @@
 import random
 import json
+from re import match
 
 RWFile = open("RecentWords.json")
 WAFile = open("WordAssociations.json")
@@ -12,5 +13,28 @@ except:
     Associations = {}
 
 #Add words to the list of recently used words, as well as add associations.
-def read():
+def read(WordString):
+    print("Reading words...")
+    global Recents
+    global Associations
+    
+    Words = WordString.split(" ")
+    for I in range(0,len(Words)-1):
+        W = Words[I]
+        Recents.append(W)
+        T = list(W)
+        print("Removing punctuation...")
+        for C in range(0,len(T)-1):
+            if not T[C].isalpha():
+                T.pop(C)
+                print(T[C])
+        if not (W in Associations):
+            Associations[W] = []
+        Associations[W].append(Words[I+1])
+
+    print(Associations)
+    
+def trim():
+    global Associations
+    global Recents
     pass
